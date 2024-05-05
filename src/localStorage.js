7c742fadd665b33b5d3f7b15b28de1c067ce073e
Yomanbest67 <106496623+Yomanbest67@users.noreply.github.com>
2024-05-05 22:39:18 +0100

    Remove old comment.

--- src/localStorage.js
+++ src/localStorage.js
@@ -50,24 +50,4 @@ function setObjects() {
      }
 }
 
-///////////////// Old function
-// function setObjects() {
-//     let newProjectList = JSON.parse(window.localStorage.getItem("projectList"));
-
-//     for (let project of newProjectList){
-//         let newProject = new Project(Object.values(project)[0], Object.values(project)[1])
-        
-//         if (project["todos"]){
-//             for (let todo of project["todos"]){
-//                 let newTodo = new toDo(todo["title"], todo["description"], todo["dueDate"], todo["priority"], todo["project"]);
-    
-//                 newProject.pushTodo(newTodo);
-//             }
-//         }
-        
-//         newProjectPush(newProject);
-//     }
-
-// }
-
 export {systemCheck, setObjects, populateStorage}
